66583aa417c94beb9effa0a2152fe1949c4f525f
Han-dragon93 <34955578+h-dragon93@users.noreply.github.com>
2019-07-28 17:50:43 +0900

    Create SW_Expert_Academy_1206.py

---
+++ SW_Expert_Academy_1206.py
@@ -0,0 +1,19 @@
+def solve(arr) :
+    count = 0
+    for i in range(2, len(arr) -2) :
+        height = arr[i]
+        left = max(arr[i-2:i])
+        if height < left :
+            continue
+        right = max(arr[i+1:i+3])
+        if height < right :
+            continue
+        possible_height = max(left, right)
+        count += (height - possible_height)
+    return count
+
+if __name__ == "__main__" :
+    for i in range(10) :
+        t = int(input().strip())
+        array = list(map(int, input().strip().split()))
+        print("#{0} {1}".format(i+1, solve(array)))
