4aff2cef6d25b14c59fa499e4da43b71e9897957
Han-dragon93 <gksdyd4238@naver.com>
2019-08-20 23:21:29 +0900

    bfs

---
+++ Baekjoon_2606.py
@@ -0,0 +1,23 @@
+def bfs(v) :
+    queue = []
+    queue.append(v)
+    visited[v] = True
+    cnt = 0
+    while queue :
+        v = queue.pop(0)
+        for e in graph[v] :
+            if not visited[e] :
+                visited[e] = True
+                queue.append(e)
+                cnt += 1
+    return cnt
+
+V = int(input())
+E = int(input())
+graph = [ [] for _ in range(V+1)]
+visited = [False for _ in range(V+1)]
+for _ in range(E) :
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+print(bfs(1))
