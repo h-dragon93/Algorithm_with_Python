65ff0695f0186f8a7fd7b4004c155e72db28a0dd
Han-dragon93 <gksdyd4238@naver.com>
2019-09-20 01:07:32 +0900

    DP

---
+++ Baekjoon_11047.py
@@ -0,0 +1,11 @@
+li = []
+count = 0
+N, K = map(int, input().split(' '))
+for i in range(N):
+    a = int(input())
+    li.append(a)
+li.sort(reverse = True)
+for i in range(len(li)) :
+    count += K//li[i]
+    K %= li[i]
+print(count)
