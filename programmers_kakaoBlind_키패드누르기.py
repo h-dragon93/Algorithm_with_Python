c24c2ec8241890340fc43d4a72fc79f1219e3a13
Han-dragon93 <gksdyd4238@naver.com>
2022-04-06 22:28:41 +0900

    Update programmers_kakaoBlind_키패드누르기.py

--- programmers_kakaoBlind_키패드누르기.py
+++ programmers_kakaoBlind_키패드누르기.py
@@ -1,10 +1,7 @@
 def solution(numbers, hand):
     answer = ''
-    # 각 버튼별 위치
     button_position = [[3, 1], [0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
-    # 왼손 초기 위치
     left = [3, 0]
-    # 오른손 초기 위치
     right = [3, 2]
  
     def set_left(number):
@@ -29,6 +26,7 @@ def solution(numbers, hand):
             # 왼손과 오른손의 거리 계산하기
             left_distance = abs(left[0] - button_position[number][0]) + abs(left[1] - button_position[number][1])
             right_distance = abs(right[0] - button_position[number][0]) + abs(right[1] - button_position[number][1])
+           
             # 거리가 같다면
             if left_distance == right_distance:
                 # 오른손잡이 일때
